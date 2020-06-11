8d30ea0976ac607ced87e60dbaec94e5e84f3dbb
Bertrand Shao <bertrandshao@gmail.com>
2020-06-10 20:50:37 -0700

    successfully started react app

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  module: {
-    rules: [
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+    publicPath: '/'
+  },
+  module : {
+    loaders : [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+        query: {
+          presets: ['react', 'env']
+       }
       }
     ]
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+  devServer: {
+    historyApiFallback: true
+  }
 };
