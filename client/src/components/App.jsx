37771a19658bddc69b9e7e063d9cd502c1da8c0b
Bertrand Shao <shaob@g.ucla.edu>
2020-06-10 20:50:37 -0700

    successfully started react app

---
+++ client/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return(
+      <Fragment>
+        <p>bertrand shao</p>
+      </Fragment>
+    );
+  };
+};
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = 9000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
