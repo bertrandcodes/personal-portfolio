ad643331198b83083611e976048632c8b1ab28f6
Bertrand Shao <shaob@g.ucla.edu>
2020-06-15 01:19:38 -0700

    addedd blog entry with css

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Resume() {
 
   return (
-    <div className="row d-flex justify-content-center pdfContainer">
+    <div className="pdfContainer">
       <iframe width="100%" height="100%" src="https://docdro.id/V9UOz0d" frameBorder="0" allowtransparency="true" allowFullScreen></iframe>
     </div>
   );
