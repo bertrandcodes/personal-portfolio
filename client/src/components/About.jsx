6f4519f8f005e033b30d63a5c89febcca320d38d
Bertrand Shao <bertrandshao@gmail.com>
2020-06-17 16:21:57 -0700

    completed homepage

---
+++ client/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+export default function About () {
+
+    return(
+      <Fragment>
+          <p>about</p>
+          <p>see my projects</p>
+      </Fragment>
+    );
+  };
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,6 +6,7 @@ import Applications from './Applications.jsx';
 import Blog from './Blog.jsx';
 import Resume from './Resume.jsx';
 import Contact from './Contact.jsx';
+import About from './About.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,6 +22,7 @@ export default class App extends React.Component {
           <Navbar />
           <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
           <Route path="/applications" component={Applications} />
           <Route path="/blog" component={Blog} />
           <Route path="/resume" component={Resume} />
