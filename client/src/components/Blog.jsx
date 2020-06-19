f3b72e238fb801c662bbfc492c251390f4411a34
Bertrand Shao <shaob@g.ucla.edu>
2020-06-19 00:03:54 -0700

    added image holder for home page

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import ScrollIndicator from './ScrollIndicator.jsx';
 
 export default function Blog() {
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,19 +7,35 @@ export default function Home() {
     <Fragment>
       <div className="background-div">
         <div className="overlay" />
-        <video autoplay="true" loop="true" muted="true">
+        <video autoPlay={true} loop={true} muted={true}>
           <source src="typing.mp4" type="video/mp4" />
           <img className="background-image" src="sample.jpg" alt="mountains and lake"></img>
         </video>
         {/* <img className="background-image" src="sample.jpg"></img> */}
+        <div className="my-face"></div>
         <div className="about-me">
           <p className="line-1 anim-typewriter">Welcome! My name is Bertrand Shao</p>
           <div className="experiences">and I develop web experiences.</div>
         </div>
-        <div className="home-button">
-          <NavLink className="btn btn-sm animated-button thar-three" to="/about">Enter</NavLink>
-          {/* <a href="#" className="btn btn-sm animated-button thar-three">Enter</a>  */}
+        <div className="button_body">
+        {/* <div className="button"> */}
+        <NavLink className="button" to="/about">
+          <div className="button__text">Enter.</div>
+
+          <div className="button__wrapper">
+            <div className="button__arrow"></div>
+            <div className="button__border-circle"></div>
+            <div className="button__mask-circle">
+              <div className="button__small-circle"></div>
+            </div>
+          </div>
+        {/* </div> */}
+        </NavLink>
         </div>
+        {/* <div className="home-button">
+          <NavLink className="btn btn-sm animated-button thar-three" to="/about">Enter</NavLink>
+        </div> */}
+
 
       </div>
     </Fragment>
