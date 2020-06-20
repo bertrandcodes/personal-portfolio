a31daecce6637ca888bda5be0874e5bcbe51bdb9
Bertrand Shao <bertrandshao@gmail.com>
2020-06-20 02:26:49 -0700

    cleaned up bugs in mobile navbar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,35 +7,19 @@ export default function Home() {
     <Fragment>
       <div className="background-div">
         <div className="overlay" />
-        <video autoPlay={true} loop={true} muted={true}>
+        <video autoplay="true" loop="true" muted="true">
           <source src="typing.mp4" type="video/mp4" />
           <img className="background-image" src="sample.jpg" alt="mountains and lake"></img>
         </video>
         {/* <img className="background-image" src="sample.jpg"></img> */}
-        <div className="my-face"></div>
         <div className="about-me">
           <p className="line-1 anim-typewriter">Welcome! My name is Bertrand Shao</p>
           <div className="experiences">and I develop web experiences.</div>
         </div>
-        <div className="button_body">
-        {/* <div className="button"> */}
-        <NavLink className="button" to="/about">
-          <div className="button__text">Enter.</div>
-
-          <div className="button__wrapper">
-            <div className="button__arrow"></div>
-            <div className="button__border-circle"></div>
-            <div className="button__mask-circle">
-              <div className="button__small-circle"></div>
-            </div>
-          </div>
-        {/* </div> */}
-        </NavLink>
-        </div>
-        {/* <div className="home-button">
+        <div className="home-button">
           <NavLink className="btn btn-sm animated-button thar-three" to="/about">Enter</NavLink>
-        </div> */}
-
+          {/* <a href="#" className="btn btn-sm animated-button thar-three">Enter</a>  */}
+        </div>
 
       </div>
     </Fragment>
