7e1510aa1d1a379e11c1ef32d5564ba1f7ecd8be
Bertrand Shao <bertrandshao@gmail.com>
2020-06-20 23:50:05 -0700

    contact form now forwards to email

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,49 @@
+require('dotenv').config();
 var helpers = require('../db/dbhelpers');
+const nodemailer = require('nodemailer');
 
 const controller = {
     post: (req, res) => {
-        var item = req.body
-        helpers
-            .post(item)
-            .then(() => res.send(200).end(console.log('posted')))
-            .catch((err) => res.send(err))
+        var data = req.body;
+        var smtpTransport = nodemailer.createTransport({
+          service: 'Gmail',
+          port: 465,
+          auth: {
+            // user: `${gmail.user}`,
+            // pass: `${gmail.password}`
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+          }
+        });
+        var mailOptions = {
+          from: data.email,
+          to: process.env.EMAIL,
+          subject: 'Message from Bertrandshao.com',
+          html: `<p>First Name: ${data.first}</p>
+                  <p>Last Name: ${data.last}</p>
+                  <p>Email: ${data.email}</p>
+                  <p>Message: ${data.message}</p>`
+        };
+        smtpTransport.sendMail(mailOptions,
+          (error, response) => {
+            if(error) {
+                console.log(error)
+              res.send(error)
+            }else {
+                console.log('success')
+              res.send('Success')
+            }
+            smtpTransport.close();
+          });
+    
       }
+    // post: (req, res) => {
+    //     var item = req.body
+    //     helpers
+    //         .post(item)
+    //         .then(() => res.send(200).end(console.log('posted')))
+    //         .catch((err) => res.send(err))
+    //   }
 }
 
 module.exports = controller;
