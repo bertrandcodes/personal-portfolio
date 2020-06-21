a433e1dc4c8c90a8c19b75a15e469a89337dbcdc
Bertrand Shao <bertrandshao@gmail.com>
2020-06-21 01:16:18 -0700

    updated SEO and added favicon

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,17 +1,22 @@
 import React, { Fragment } from 'react';
+import { Helmet } from 'react-helmet';
 
-export default function About () {
+export default function About() {
 
-    return(
-      <div id="about-body">
+  return (
+    <div id="about-body">
+      <Helmet>
+        <title>About Me | Bertrand Shao</title>
+        <meta name="description" content="Learn about who I am and why I love what I do."></meta>
+      </Helmet>
       <div className="about-main">
-          <div className="about-image"/>
-          <div className="about-border">
-            <div className="about-text">
-              
-            </div>
+        <div className="about-image" />
+        <div className="about-border">
+          <div className="about-text">
+
           </div>
+        </div>
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
