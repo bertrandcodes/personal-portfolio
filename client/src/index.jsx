6ff2f18f00c3d17c666c2f3f0c6b571269407b6b
Bertrand Shao <bertrandshao@Bertrands-MacBook-Pro.local>
2020-06-26 15:33:36 -0700

    Remove Redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,10 @@ import App from "./components/App.jsx";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./redux/reducers/index.js";
-import burgerMenu from "./redux/reducers/burgerMenu.js";
+// import burgerMenu from "./redux/reducers/burgerMenu.js";
 
 const store = createStore(
-  rootReducer,
-  { burgerMenu } + window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer
 );
 
 ReactDOM.render(
