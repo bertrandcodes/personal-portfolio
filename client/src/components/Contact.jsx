9bb310f4c59a7c6934c2bdbf60204df6018333b9
Bertrand Shao <bertrandshao@Bertrands-MacBook-Pro.local>
2020-06-27 01:07:46 -0700

    Add netlify tag

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -46,7 +46,7 @@ export default class Contact extends React.Component {
   render() {
     const { name, email, message } = this.state;
     return (
-      <form onSubmit={this.handleSubmit} method="post" netlify>
+      <form onSubmit={this.handleSubmit}>
         <p>
           <label>
             Your Name:{" "}
