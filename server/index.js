26336e9286c71e0189a4bae94660313fcbd6aba6
Bertrand Shao <bertrandshao@gmail.com>
2020-06-26 22:47:38 -0700

    Update contact functionality through Netlify

--- server/index.js
+++ server/index.js
@@ -10,13 +10,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+//       if (err) {
+//         res.status(500).send(err)
+//       }
+//     })
+//   })
 
 // app.use('/', router)
 
