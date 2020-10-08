207687f178f8366b099752ea7ffc02849c645700
Bertrand Shao <shaob@g.ucla.edu>
2020-10-08 00:53:10 -0700

    Add production mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
