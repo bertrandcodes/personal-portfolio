da639e2af142f82e70a40df3794b0f558958ddd8
Bertrand Shao <bertrandshao@gmail.com>
2020-10-09 12:19:56 -0700

    Production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,46 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR,
-    publicPath: '/'
-  },
-  plugins: [new MiniCssExtractPlugin()],
-  optimization: {
-    minimizer: [new UglifyJsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        include: SRC_DIR,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/react', '@babel/env']
+module.exports = env => {
+  return {
+    mode: 'production',
+    entry: `${SRC_DIR}/index.jsx`,
+    output: {
+      filename: 'bundle.js',
+      path: DIST_DIR,
+      publicPath: '/'
+    },
+    plugins: [new MiniCssExtractPlugin(), new BundleAnalyzerPlugin()],
+    optimization: {
+      minimizer: [new UglifyJsPlugin(), new TerserPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          include: SRC_DIR,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/react', '@babel/env']
+            }
           }
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true
+      ]
+    },
+    devServer: {
+      historyApiFallback: true
+    }
   }
 };
