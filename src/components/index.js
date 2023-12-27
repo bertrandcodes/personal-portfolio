0d97479a9d601e3cc5e2818edc2e81ea18583e64
Bertrand Shao <bertrandshao@Bertrands-MacBook.local>
2023-12-26 18:11:15 -0800

    fix tsconfig path issue

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SeoData = void 0;
-/* eslint-disable import/prefer-default-export */
-var SeoData_1 = require("./SeoData");
-Object.defineProperty(exports, "SeoData", { enumerable: true, get: function () { return __importDefault(SeoData_1).default; } });
