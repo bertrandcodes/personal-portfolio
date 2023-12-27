4b26e0e05b55966c0dc2d32ac661f1f7aa6016e2
Bertrand Shao <bertrandshao@gmail.com>
2023-12-26 21:30:57 -0800

    revamp home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,43 +2,37 @@ import React from "react";
 import is from "is_js";
 import { NavLink } from "react-router-dom";
 import { SeoData } from "@components";
+import HomeButton from "./components/Button";
 import styles from "./styles.module.scss";
 
+const MountainBackground = () => (
+  <img className={styles.image} src="mountainlake.jpg" alt="mountains and lake" />
+);
+
 const Home = () => (
-  <div className="main">
+  <div className={styles.root}>
     <SeoData
       title="Home | Bertrand Shao"
       name="description"
       content="Begin your journey here. Find out about the latest creative web projects I've been working on. Try out some cool applications."
     />
-    <div className="background-div">
-      <div className="overlay" />
+    <div className={styles.background}>
+      <div className={styles.overlay} />
       {is.mobile() ? (
-        <img className="background-image" src="mountainlake.jpg" alt="mountains and lake" />
+        <MountainBackground />
       ) : (
-        <video className="home-video" autoPlay loop muted>
+        <video className={styles.video} autoPlay loop muted>
           <source src="code.mp4" type="video/mp4" />
-          <img className="background-image" src="mountainlake.jpg" alt="mountains and lake" />
+          <MountainBackground />
         </video>
       )}
-      <div className="home-body">
-        <NavLink className="my-face" to="/about" />
-        <div className="about-me">
-          <p className="line-1 anim-typewriter">Welcome! My name is Bertrand Shao</p>
-          <div className="experiences">and I develop web experiences.</div>
+      <div className={styles.text}>
+        <NavLink className={styles.face} to="/about" />
+        <div className={styles.about}>
+          <p className={styles.line1}>Welcome! My name is Bertrand Shao</p>
+          <div className={styles.line2}>and I develop web experiences.</div>
         </div>
-        <div className="button_body">
-          <NavLink className="button" to="/about">
-            <div className="button__text">Enter.</div>
-            <div className="button__wrapper">
-              <div className="button__arrow" />
-              <div className="button__border-circle" />
-              <div className="button__mask-circle">
-                <div className="button__small-circle" />
-              </div>
-            </div>
-          </NavLink>
-        </div>
+        <HomeButton />
       </div>
     </div>
   </div>
