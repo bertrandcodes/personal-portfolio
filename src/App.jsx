63ca23716076257e4c7c2590721db86ada2df07e
Bertrand Shao <shaob@g.ucla.edu>
2023-12-28 20:27:22 -0800

    move navbar component to component directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Home, Navbar } from "@pages";
+
+import { Home } from "@pages";
+import { Navbar, Footer } from "@components";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Applications from "./components/Applications.jsx";
@@ -10,7 +12,6 @@ import About from "./components/About.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import NotFound from "./components/NotFound.jsx";
 import "./style.scss";
-import { Footer } from "@components";
 
 export default class App extends React.Component {
   constructor(props) {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as SeoData } from "./SeoData";
 export { default as Footer } from "./Footer";
+export { default as Navbar } from "./Navbar";
