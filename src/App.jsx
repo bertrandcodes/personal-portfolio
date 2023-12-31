198b145e1e4dd39a738352c9a72f13277d97b18d
Bertrand Shao <shaob@g.ucla.edu>
2023-12-30 17:56:41 -0800

    finish contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import { Home, Applications } from "@pages";
+import { Home, Applications, Contact } from "@pages";
 import { Navbar, Footer } from "@components";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Blog from "./components/Blog.jsx";
 import Resume from "./components/Resume.jsx";
-import Contact from "./components/Contact.jsx";
 import About from "./components/About.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 import NotFound from "./components/NotFound.jsx";
