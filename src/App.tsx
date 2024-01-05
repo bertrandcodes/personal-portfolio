1b1c7b626f7e7ab33cb536617dfba935593be2c2
Bertrand Shao <bertrandshao@gmail.com>
2024-01-05 00:13:40 -0800

    add new blog pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,9 @@ const App = () => (
       <Route path="/applications" component={Applications} />
       <Route path="/blog" exact component={Blog} />
       <Route path="/blog/sdc" component={SDC} />
+      <Route path="/blog/restocking" component={SDC} />
+      <Route path="/blog/payment" component={SDC} />
+      <Route path="/blog/logger" component={SDC} />
       <Route path="/resume" component={Resume} />
       <Route path="/contact" component={Contact} />
       <Route component={NotFound} />
