1204fabe8d79f5ef5fe65f00ef07a060624dd2e3
Bertrand Shao <shaob@g.ucla.edu>
2024-01-12 02:36:21 -0800

    remove extra lines

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -7,9 +7,9 @@ const About = () => (
     <div className={styles.image} />
     <h1 className={styles.header}>A little bit about me...</h1>
     <p className={styles.text}>
-      In 2018, while waiting for a developer to patch a bug in my non-technical role, I tried to fix
-      the issue myself. The result was my first line of code and a passion for programming that has
-      been growing ever since.
+      In 2018, while waiting for a developer to patch a bug from my non-technical role, I tried to
+      fix the issue myself. The result was my first line of code and a passion for programming that
+      has been growing ever since.
     </p>
     <p className={styles.text}>
       In 2020, I joined an early-stage{" "}
@@ -55,7 +55,6 @@ const About = () => (
       from their school&apos;s curriculum. Through the same program, I enjoy mentoring students
       entering college one-on-one, preparing them for academia and future career endeavors.
     </p>
-
     <p className={styles.text}>
       Outside of work, I am an active member and the current president of a Toastmasters club.
       Public speaking has been a significant journey for me, and I find great fulfillment in helping
@@ -63,7 +62,6 @@ const About = () => (
       my passion for public speaking and develop platforms that will enable people to build their
       social skills from the comfort of their own homes.
     </p>
-
     <p className={styles.text}>
       In my free time, you can find me playing basketball, hip-hop dancing, or doodling in my
       sketchbook.
