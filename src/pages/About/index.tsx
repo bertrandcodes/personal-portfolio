dba18dc0fbc50bdc69cb4a4ad170f93f0be4a5da
Bertrand Shao <bertrandshao@gmail.com>
2024-01-12 02:34:57 -0800

    clean up about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,27 +8,52 @@ const About = () => (
     <h1 className={styles.header}>A little bit about me...</h1>
     <p className={styles.text}>
       In 2018, while waiting for a developer to patch a bug in my non-technical role, I tried to fix
-      the issue myself. The result was my first line of code and a passion that has been growing
-      ever since.
+      the issue myself. The result was my first line of code and a passion for programming that has
+      been growing ever since.
     </p>
     <p className={styles.text}>
-      In 2020, I joined an early-stage delivery startup as the third full-stack engineer and dove
-      headfirst into ideating, developing, and maintaining features across all facets of the
-      product. Given the fast paced nature I took on various ad hoc roles as well, ranging from
-      designer to DevOps to warehouse worker. Over my tenure, the company expanded from a single
-      location to seven across three different schools and grew to an estimated $30 million
-      valuation. In 2022, I transitioned to a much larger software enterprise environment at Optum,
-      where I currently work with high-risk transactional data and provide user account
-      functionality to over 20 million monthly users.
+      In 2020, I joined an early-stage{" "}
+      <a href="https://www.duffl.com/" target="_blank" rel="noreferrer">
+        delivery startup
+      </a>{" "}
+      as the third full-stack engineer and dove headfirst into ideating, developing, and maintaining
+      features across all facets of the product. Given the fast paced nature I took on various ad
+      hoc roles as well, ranging from designer to DevOps to warehouse worker. Over my tenure, the
+      company expanded from a single location to seven across three different schools and has grown
+      to an estimated $30 million valuation. In 2022, I transitioned to a much larger software
+      enterprise environment at{" "}
+      <a href="https://www.myuhc.com/" target="_blank" rel="noreferrer">
+        Optum
+      </a>
+      , where I currently work with high-risk transactional data and provide account functionalities
+      to over 20 million monthly users.
     </p>
     <p className={styles.text}>
-      I have had the privilege of participating on panels at Career Karma, MissionBit, and Hack
-      Reactor, detailing my journey breaking into the tech industry and sharing tips on how to
-      succeed in both the job search and role itself. I frequently lead coding classes and workshops
-      through a program called MissionBit, whose purpose is to provide high school students with the
-      computer science education not taught in their school&apos;s curriculum. Through the same
-      program, I enjoy mentoring students entering college one-on-one, preparing them for academia
-      and future career endeavors.
+      I have had the privilege of participating on panels at{" "}
+      <a href="https://careerkarma.com/" target="_blank" rel="noreferrer">
+        Career Karma
+      </a>
+      ,{" "}
+      <a href="https://missionbit.org/" target="_blank" rel="noreferrer">
+        MissionBit
+      </a>
+      , and{" "}
+      <a
+        href="https://www.galvanize.com/explore-hack-reactor-coding-bootcamps/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Hack Reactor
+      </a>{" "}
+      where I have detailed my journey breaking into the tech industry and shared tips on succeeding
+      in both the job search and role itself. I frequently lead coding classes and workshops through
+      a program called{" "}
+      <a href="https://missionbit.org/" target="_blank" rel="noreferrer">
+        MissionBit
+      </a>
+      , whose purpose is to provide high school students with the computer science education absent
+      from their school&apos;s curriculum. Through the same program, I enjoy mentoring students
+      entering college one-on-one, preparing them for academia and future career endeavors.
     </p>
 
     <p className={styles.text}>
