3b83567624a1b5b3af07eadd02785142b1c1e7ba
Bertrand Shao <bertrandshao@gmail.com>
2024-01-12 12:29:23 -0800

    remove built images

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "images/",
             },
           },
         ],
