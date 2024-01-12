b37b3811ed074ac447a7bbaeef179f0565e16dd7
Bertrand Shao <bertrandshao@gmail.com>
2024-01-12 00:34:56 -0800

    clean up index.scss

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,46 +1,46 @@
 import React from "react";
+
 import styles from "./styles.module.scss";
 
 const About = () => (
   <div className={styles.root}>
-    <img className={styles.image} src="/me.jpg" alt="bertrand in mexico city" />
-    <div className="about-paragraphs-first">I’ve always been taught to try new things.</div>
-    <div className="about-paragraphs">
-      Growing up in NYC, I practiced fine arts and played computer games. As a teen, I played a
-      variety of sports and my favorite subject was Math. Upon attending UCLA, I studied
-      psychobiology and participated on a collegiate dance team. I truly loved everything I took an
-      interest to. “But what do you want to do for a living?” For some reason, I just didn’t know.
-    </div>
-    <div className="about-paragraphs">
-      The same feeling that would pique my interest was the same feeling that left me unsatisfied.
-      My hobbies ranged far and wide and yet they always seemed at odds with one another. My mind
-      worked well with numbers and data while my heart yearned for a creative outlet. I loved team
-      dynamics, but I worked best solo. The concept of fitting into a <span>single</span> role for
-      life seemed farfetched.
-    </div>
-    <div className="about-paragraphs">
-      Working in Search Engine Optimization (SEO), I felt the closest yet to my calling. Keeping up
-      with changing technology was a thrill. Using creative skills to churn out graphics and fresh
-      content scratched that itch to create. Working in technologies felt natural after a lifetime’s
-      worth of Runescape hours. But whenever a problem arose we always had to contact a developer.
-      Most times they were slow to respond and we would be helpless against a bug. So one day I
-      thought- why not me? Perhaps I could fix it instead of waiting. So I dug around a bit. Then
-      dug deeper. And deeper. And deeper…
-    </div>
-    <div className="about-paragraphs">
-      I don’t think I found a fix to that bug that day. But what I found instead was an answer to a
-      lifetime of uncertainty. I discovered an interest in looking at code. That led to an interest
-      in playing with it. Before long I was exposed to a field with endless paths and a hobby with
-      infinite creative potential. Here was a world where I could use my logic to wrestle large and
-      complex puzzles- some of which have never been solved before. At the same time I’m offered a
-      digital canvas and unlimited mediums to paint with. So I started practicing.
-    </div>
-    <div className="about-paragraphs">I haven’t looked back since.</div>
-    <div className="about-paragraphs">
-      I’ve worn many hats in life, but none quite fit me like software engineering. All that’s left
-      now is to follow this road while honing my craft and enjoying it each and every day. A
-      lifetime’s worth of journeys await.
-    </div>
+    <img className={styles.image} src="/me.jpg" alt="me in Mexico City" />
+    <p>
+      In 2018, while waiting for a developer to patch a bug in my non-technical role, I decided to
+      see if I could do it myself. The result was my first line of code and a passion that has been
+      growing ever since.
+    </p>
+    <p>
+      In 2020, I joined an early-stage delivery startup as the third full-stack engineer, rapidly
+      ideating, developing, and maintaining features across all angles of the product. Given the
+      fast paced nature I took on various ad hoc roles as well, ranging from designer to DevOps to
+      warehouse worker. Over my tenure, the company expanded from a single location to seven across
+      three different schools and grown to an estimated $30 million valuation. In 2022, I
+      transitioned to a much larger software enterprise environment at Optum, where I currently work
+      with high-risk transactional data and provide user account functionality to over 20 million
+      monthly users.
+    </p>
+    <p>
+      I have had the privilege of being a panelist for Career Karma, MissionBit, and HackReactor,
+      detailing my journey of breaking into the tech industry and sharing tips on how to succeed in
+      both the job search and role itself. I frequently lead coding workshops through a program
+      called MissionBit, whose purpose is to provide high school students with the computer science
+      education not taught in their curriculums. Through the same program, I enjoy mentoring
+      students entering college one-on-one, preparing them for school and future career endeavors.
+    </p>
+
+    <p>
+      Outside of work, I am an active member and the current president of my Toastmasters club.
+      Public speaking has been a significant journey for me, and I find great reward in helping
+      others reach their goals. My dream is to combine my technical abilities in software with my
+      passion for public speaking to develop platforms that will enable people to build their social
+      skills from the comfort of their own homes.
+    </p>
+
+    <p>
+      In my free time, you can find me playing basketball, hip-hop dancing, or drawing in my
+      sketchbook.
+    </p>
   </div>
 );
 
